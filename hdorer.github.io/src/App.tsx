3d273b4993dbbae6d023cf7670d7eaa48d8b0ef4
hdorer <41219697+hdorer@users.noreply.github.com>
2024-08-03 19:08:17 -0400

    Create React app, clear most of the default stuff, added CSS from website, created Footer component

---
+++ hdorer.github.io/src/App.tsx
@@ -0,0 +1,11 @@
+import Footer from "./components/footer";
+
+function App() {
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ hdorer.github.io/src/components/footer.tsx
@@ -0,0 +1,28 @@
+import githubIcon from '../assets/github-logo.png';
+import itchioIcon from '../assets/itch-io-icon.png';
+import linkedinIcon from '../assets/linkedin-logo.png';
+import emailIcon from '../assets/email-icon.png'
+
+
+function Footer() {
+    return (
+        <footer>
+            <div className="footer-container">
+                <a href="https://github.con/hdorer" className="footer-link">
+                    <img src={githubIcon} alt="Look at my repositories on GitHub"></img>
+                </a>
+                <a href="https://hdorer.itch.io/" className="footer-link">
+                    <img src={itchioIcon} alt="Play my games on itch.io"></img>
+                </a>
+                <a href="https://www.linkedin.com/in/harrydorer" className="footer-link">
+                    <img src={linkedinIcon} alt="Connect with me on LinkedIn"></img>
+                </a>
+                <a href="mailto:hdorer24@gmail.com" className="footer-link">
+                    <img src={emailIcon} alt="Email me"></img>
+                </a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
