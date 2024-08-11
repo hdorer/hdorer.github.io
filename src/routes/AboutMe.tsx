ae6bc1c27b1c991241576dd13296b5b2efb45fdf
hdorer <41219697+hdorer@users.noreply.github.com>
2024-08-10 21:44:14 -0400

    Successfully loaded one Markdown file into the webpage

--- src/routes/AboutMe.tsx
+++ src/routes/AboutMe.tsx
@@ -1,7 +1,31 @@
-function AboutMe() {
+import { useState, useEffect } from 'react';
+import Markdown from 'react-markdown';
+import './page.css';
+
+function AboutMe() {    
+    const [aboutMeText, setAboutMeText] = useState('');
+
+    useEffect(() => {
+        const markdownFiles = import.meta.glob('../assets/content/About Me.md', { as: 'raw' });
+
+        const loadMarkdown = async () => {
+            const file = markdownFiles['../assets/content/About Me.md'];
+            const content = await file();
+            console.log(content);
+            setAboutMeText(content);
+        };
+
+        loadMarkdown();
+    }, []);
+
     return (
-        <div className="content-text">
-            <p>Oh man this sure is a webpage</p>
+        <div className="page-container">
+            <div className="content-container">
+                <h1 className="page-itle">Hi!  I'm Harry Dorer.</h1>
+                <div className="content-block">
+                    <Markdown className="content-text">{aboutMeText}</Markdown>
+                </div>
+            </div>
         </div>
     );
 }
