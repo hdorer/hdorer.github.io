ed763661a2d4707c72d687fff73d5b8d17d2c549
hdorer <41219697+hdorer@users.noreply.github.com>
2024-08-11 18:01:26 -0400

    Attempt 2 to configure deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hdorer.github.io/',
+  base: '/hdorer-github-io/',
   plugins: [react()],
-  assetsInclude: ["**/*.md"]
+  assetsInclude: ["**/*.md"],
+  build: {
+    outDir: 'dist'
+  }
 })
