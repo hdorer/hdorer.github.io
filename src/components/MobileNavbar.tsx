10a5433828574702385325e1d95945432c2eb12d
hdorer <41219697+hdorer@users.noreply.github.com>
2024-08-12 23:12:02 -0400

    Fix video player width

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -10,12 +10,12 @@ function MobileNavbar() {
     return (
         <header>
             <div className="mobile-nav">
-                <div className={ opened ? "mobile-nav-links opened" : "mobile-nav-links"}>
+                <div className={ opened ? "mobile-nav-links opened" : "mobile-nav-links" }>
                     <NavbarLinks onClick={ () => setOpened(false) } />
                 </div>
                 <div className="mobile-nav-menu-icon">
                     <a onClick={ () => setOpened(!opened) }>
-                        <img src={opened ? closeIcon : hamburgerIcon} />
+                        <img src={ opened ? closeIcon : hamburgerIcon } />
                     </a>
                 </div>
             </div>
--- src/routes/Projects.tsx
+++ src/routes/Projects.tsx
@@ -1,8 +1,23 @@
+import { useState, useEffect } from 'react';
 import Markdown from 'react-markdown';
 import projectsContent from '../assets/content/Encounter System Article.md?raw';
 import './page.css';
 
 function Projects() {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, []);
+
     return (
         <div className="page-container">
             <div className="content-container">
@@ -10,8 +25,8 @@ function Projects() {
                 <div className="content-block">
                     <Markdown className="content-text">{projectsContent}</Markdown>
                     <iframe
-                      width="560"
-                      height="315"
+                      width={ width >= 768 ? "560" : "256" }
+                      height={ width >= 768 ? "315" : "144" }
                       src="https://www.youtube.com/embed/gpjXHFgNoHk?si=CgkM1-BCLZ0tpkCu"
                       title="YouTube video player"
                       frameBorder="0"
