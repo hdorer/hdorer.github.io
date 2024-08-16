413a37ef202246891d9e44ca19fed4520a6d2859
hdorer <41219697+hdorer@users.noreply.github.com>
2024-08-16 02:01:05 -0400

    added mobile card style

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,7 +4,7 @@ import './Card.css';
 interface Props {
     title?: string;
     thumbnail?: string;
-    children: ReactNode;
+    children?: ReactNode;
 }
 
 function Card({ title, thumbnail, children }: Props) {
