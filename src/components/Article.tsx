4aee51b139443e7ba75e2553ea3e2b066365cb10
hdorer <41219697+hdorer@users.noreply.github.com>
2024-09-07 02:27:48 -0500

    Fully fleshed out Paragraph component, data structure

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,55 +1,86 @@
 import { useState, useEffect } from 'react';
 import Markdown from 'react-markdown';
 import articles from '../modules/ArticleLoader';
-import portrait from '../assets/images/portrait.jpg';
 import './Article.css';
 
+interface ArticleImage {
+    src: string;
+    paragraph: number;
+    caption?: string;
+}
+
 export interface ArticleData {
-    filename: string
+    filename: string;
+    images: ArticleImage[];
+}
+
+interface ParagraphProps {
+    text: string;
+    imageSrc?: string;
+    imageCaption?: string;
 }
 
 interface Props {
     data: ArticleData;
 }
 
+function Paragraph({ text, imageSrc, imageCaption }: ParagraphProps) {
+    return (
+        <div className="paragraph">
+            <div className={imageSrc ? "text-column" : "text-column no-image"}>
+                <Markdown className="article-text">{text}</Markdown>
+            </div>
+            {imageSrc && (
+                <div className="media-column">
+                    <img src={`/src/assets/images/${imageSrc}`} />
+                    {imageCaption && <p className="image-caption">{imageCaption}</p>}
+                </div>
+            )}
+        </div>
+    );
+}
+
 export function Article({ data }: Props) {
     const [paragraphStrings, setParagraphStrings] = useState<string[]>();
     const [success, setSuccess] = useState<boolean>(true);
 
-    const loadArticle = async (): Promise<string[]> => {
-        const importArticle = articles[`../assets/articles/${data.filename}`];
-        if(!importArticle) {
-            throw new Error(`Article ${data.filename} not found!`);
+    useEffect(() => {
+        const loadArticle = async (): Promise<string[]> => {
+            const importArticle = articles[`../assets/articles/${data.filename}`];
+            if(!importArticle) {
+                throw new Error(`Article ${data.filename} not found!`);
+            }
+            
+            const importedArticle = await importArticle();
+    
+            return importedArticle.split('\n\n');
         }
-        
-        const importedArticle = await importArticle();
-
-        return importedArticle.split('\n\n');
-    }
-
-    loadArticle().then(paragraphs => {
-        setParagraphStrings(paragraphs);
-        setSuccess(true);
-    }).catch(error => {
-        console.error(`Error loading article ${data.filename}: ` + error);
-        setSuccess(false);
-    });
+    
+        loadArticle().then(paragraphs => {
+            setParagraphStrings(paragraphs);
+            setSuccess(true);
+        }).catch(error => {
+            console.error(`Error loading article ${data.filename}: ` + error);
+            setSuccess(false);
+        });
+    }, []);
 
     if(!success) {
-        return <p className="article-text">There was an error loading this article!</p>
+        return <p className="article-text">There was an error loading this article!</p>;
     }
 
     return (
         <article>
-            <div className="text-column">
-                {paragraphStrings?.map((paragraph, index) => (
-                    <Markdown key={index} className="article-text">{paragraph}</Markdown>
-                ))}
-            </div>
-            <div className="media-column">
-                <img src={portrait} />
-                <p className="image-caption">Image caption</p>
-            </div>
+            {paragraphStrings?.map((paragraph, index) => {
+                const imageObj = data.images.find(obj => obj.paragraph === index);
+                if(!imageObj) {
+                    console.log(`No image object found for paragraph ${index}!`)
+                    return <Paragraph key={index} text={paragraph} />;
+                }
+                
+                console.log(imageObj);
+                return <Paragraph key={index} text={paragraph} imageSrc={imageObj.src} imageCaption={imageObj.caption} />;
+            })}
         </article>
     );
 }
